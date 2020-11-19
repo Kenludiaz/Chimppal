51371191742bfb651c775e25f0ba398c5bddb62b
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-11-19 10:28:10 -0500

    Lab 13

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const dispNav = () => {
 }
 
 hamburger.addEventListener("click", dispNav);
+//This code was implemented for Lab 13
+const friendList = document.querySelector(".call-to-action-btn")
+friendList.addEventListener("click", () => {
+    //Inner HTML alters the previous html
+    friendList.innerHTML = "Congratulations for becoming a friend!!";
+});
+
+const changeColor = document.querySelector('.header');
+changeColor.addEventListener("click", () => {
+    //Chose to toggle class so that the user may switch back to light blue
+    document.querySelector('.header').classList.toggle("white");
+})
